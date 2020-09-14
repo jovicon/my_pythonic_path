2853c658c45e3f2ca7801929abfd09fae7eee2be
JoseContreras <jocontrera16@gmail.com>
2020-09-13 21:21:38 -0300

    applying Inheritance to toy class

--- courses/real_python/oop_path/intro_oop/classes/toy.py
+++ courses/real_python/oop_path/intro_oop/classes/toy.py
@@ -2,9 +2,8 @@ from classes.brand import Brand
 
 class Toy:
 
-    sound = True
+    sound = False
     inbox = True
-    battery = False
 
     def __init__(self, name: str, color: str, brand: Brand, price: int, size: float):
         self.name = name
@@ -13,8 +12,23 @@ class Toy:
         self.brand = brand
         self.size = size # meters
 
+    def walk(self):
+        return '{} is walking.'.format(self.name)
+
+    def action(self):
+        return '{} is scratching.'.format(self.name)
+
+
+class ToyWithSound(Toy):
+
+    sound = True
+    battery = False
+
     def speech_button(self):
         if self.sound and self.battery:
             return 'Hi! My name is {}, I want to play with you. I really love to play.'.format(self.name)
         else:
             return 'you have to buy batteries kid.'
+
+    def action(self):
+        return '{} is punching.'.format(self.name)
--- courses/real_python/oop_path/intro_oop/main.py
+++ courses/real_python/oop_path/intro_oop/main.py
@@ -1,19 +1,29 @@
 from classes.brand import Brand
-from classes.toy import Toy
-
+from classes.toy import Toy, ToyWithSound
 
 mattel = Brand("Mattel","This a great toys brand")
-spiderman = Toy("Spiderman", "Red", mattel, 123456, 1.56)
+wolverine = Toy("wolverine", "Yellow", mattel, 123456, 1.56)
+spiderman = ToyWithSound("Spiderman", "Red", mattel, 123456, 1.56)
 
-print("***********************************************************************************")
-print("***************************** TOY DESCRIPTION *************************************")
-print("***********************************************************************************")
+print("***************************************************************************************")
+print("***************************** TOY DESCRIPTION - 1 *************************************")
+print("***************************************************************************************")
 print(f'This is the toy you are buying {spiderman.name}')
 print(f'This is the toy brand {spiderman.brand.name}')
 print(f'This is the toy brand description {spiderman.brand.description}')
 print(f'This is the toy price {spiderman.price}')
 print(f'This is the toy size {spiderman.size}')
+print(f'This is the toy size {spiderman.action()}')
 
+print("***************************************************************************************")
+print("***************************** TOY DESCRIPTION - 1 *************************************")
+print("***************************************************************************************")
+print(f'This is the toy you are buying {wolverine.name}')
+print(f'This is the toy brand {wolverine.brand.name}')
+print(f'This is the toy brand description {wolverine.brand.description}')
+print(f'This is the toy price {wolverine.price}')
+print(f'This is the toy size {wolverine.size}')
+print(f'This is the toy size {wolverine.action()}')
 
 print(spiderman.speech_button())
 

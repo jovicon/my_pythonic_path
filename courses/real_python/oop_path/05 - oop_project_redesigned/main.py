cc264ac7dc186de1426b51aae2ced975c3192175
JoseContreras <jocontrera16@gmail.com>
2020-09-27 21:34:46 -0300

    composition Flexibility

--- courses/real_python/oop_path/05 - oop_project_redesigned/main.py
+++ courses/real_python/oop_path/05 - oop_project_redesigned/main.py
@@ -1,37 +1,50 @@
-import hr
-import employees
-import productivity
-import contacts
+from hr import PayrollSystem
+from productivity import ProductivitySystem
+from employees import EmployeeDataBase
 
-manager = employees.Manager(1, 'Jose Contreras', 1500)
-manager.address = contacts.Address(
-    '121 Admin Road',
-    'Concord',
-    'NH',
-    '03301'
-)
+productivity_system = ProductivitySystem()
+payroll_system = PayrollSystem()
+employee_database = EmployeeDataBase()
 
-secretary = employees.Secretary(1, 'Laura Vergara', 750)
-secretary.address = contacts.Address(
-    '121 Admin Road',
-    'Concord',
-    'NH',
-    '03301'
-)
+employees = employee_database.employees()
+productivity_system.track(employees, 40)
+payroll_system.calculate_payroll(employees)
 
 
-sales_guy = employees.SalesPerson(3, 'Pablo Espinoza', 1000, 250)
-factory_worker = employees.FactoryWorker(4, 'Gustavo Cumare', 40, 20)
-temporary_secretary = employees.TemporarySecretary(5,'Fabiola Barrueta', 40, 9)
+# import hr
+# import employees
+# import productivity
+# import contacts
 
-# we comment this because Employee is an Abstract Class, so It can be instaciated
-# generic_employee = hr.Employee(4, 'Generic Employee')
+# manager = employees.Manager(1, 'Jose Contreras', 1500)
+# manager.address = contacts.Address(
+#     '121 Admin Road',
+#     'Concord',
+#     'NH',
+#     '03301'
+# )
 
-employees_list = [ manager, secretary, sales_guy, factory_worker, temporary_secretary ]
+# secretary = employees.Secretary(1, 'Laura Vergara', 750)
+# secretary.address = contacts.Address(
+#     '121 Admin Road',
+#     'Concord',
+#     'NH',
+#     '03301'
+# )
 
 
-productivity_system = productivity.ProductivitySystem()
-productivity_system.track(employees_list, 40)
+# sales_guy = employees.SalesPerson(3, 'Pablo Espinoza', 1000, 250)
+# factory_worker = employees.FactoryWorker(4, 'Gustavo Cumare', 40, 20)
+# temporary_secretary = employees.TemporarySecretary(5,'Fabiola Barrueta', 40, 9)
 
-payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(employees_list)
+# # we comment this because Employee is an Abstract Class, so It can be instaciated
+# # generic_employee = hr.Employee(4, 'Generic Employee')
+
+# employees_list = [ manager, secretary, sales_guy, factory_worker, temporary_secretary ]
+
+
+# productivity_system = productivity.ProductivitySystem()
+# productivity_system.track(employees_list, 40)
+
+# payroll_system = hr.PayrollSystem()
+# payroll_system.calculate_payroll(employees_list)
